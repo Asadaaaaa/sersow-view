9785278e1366c3420eaad71ed80d5a17c938caf9
zkazharan <azka.azharan@gmail.com>
2023-04-04 02:47:25 +0700

    test commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}<p>test</p></body>
+      <body>{children}<p>test commit</p></body>
     </html>
   )
 }
