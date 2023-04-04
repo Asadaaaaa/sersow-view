25bc5644dee7ae81e870fe15a909921d2a7feb11
zkazharan <azka.azharan@gmail.com>
2023-04-04 17:07:12 +0700

    NextJS 13

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,17 @@
-import './globals.css'
+import './globals.css';
+import './font.module.css';
 
 export const metadata = {
   title: 'Sersow',
   description: 'Sersow is a project showcase app for uploading and showcasing their project work, interact with others, seek ideas and find collaborators or teams.'
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}<p>test commit</p></body>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
-import Image from 'next/image'
-import { Lexend_Deca } from 'next/font/google'
-import styles from './page.module.css'
-
-const lexen = Lexend_Deca({ subsets: ['latin'] })
+import styles from "./font.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>Sersow</h1>
-        <p className={lexen.className}>Sersow is a project showcase app for uploading and showcasing their project work, interact with others, seek ideas and find collaborators or teams.</p>
+    <main>
+      <div>
+        <h1>Landing page</h1>
       </div>
     </main>
-  )
+  );
 }
