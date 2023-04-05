e4780467967a0238f8d3453fc65f9542df65eafb
zkazharan <azka.azharan@gmail.com>
2023-04-05 23:18:15 +0700

    Initiate Coming Soon

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import font from './font.module.css';
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1 className={`${font.Clash_display_d6bold} text-red-500`}>Landing page Sersow</h1>
-        <p>Sersow is a project showcase app for uploading and showcasing their project work, interact with others, seek ideas and find collaborators or teams.</p>
+      <div className="py-20">
+        <h1 className={`${font.Clash_display_d6bold} text-red-500 text-center`}>Coming Soon Page</h1>
+        <p className="text-center">Sersow is a project showcase app for uploading and showcasing their project work, interact with others, seek ideas and find collaborators or teams.</p>
       </div>
     </main>
   );
--- src/app/register/StepOne.js
+++
@@ -1,5 +0,0 @@
-export default function StepOne() {
-	return (
-		<div></div>
-	);
-}
