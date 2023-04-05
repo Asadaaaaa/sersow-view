9702dcb9cf6c1dafedf096447b21812d4bd9f395
zkazharan <azka.azharan@gmail.com>
2023-04-05 22:52:13 +0700

    Edit bg-gradient

---
+++ src/app/register/StepOne.js
@@ -0,0 +1,5 @@
+export default function StepOne() {
+	return (
+		<div></div>
+	);
+}
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import StepOne from './StepOne';
 import font from '../font.module.css';
 import styles from '../globals.css';
 
@@ -6,9 +7,11 @@ export default function Register() {
   return (
 		<main>
 			<Header />
-      <div className="bg-slate-950 relative h-screen">
+      <div className="bg-slate-950 relative h-screen overflow-hidden">
 				<div className="bg-gradient"></div>
-        <h1 className={`${font.Clash_display_d6bold} text-red-500`}>Register Page Sersow</h1>
+        <div className="flex flex-col items-center">
+					<StepOne />
+				</div>
       </div>
     </main>
 	);
