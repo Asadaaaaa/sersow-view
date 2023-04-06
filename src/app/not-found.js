546e7d0ed68167df6179710e13f5f77d6b454a92
Innaka <innakadyleexd@gmail.com>
2023-04-06 20:35:55 +0700

    Edit 404 Text

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import font from './font.module.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 import RGradient from '../../public/images/rGradient.svg';
 import LGradient from '../../public/images/lGradient.svg';
-import Footer from './components/Footer';
+import IconNol from '../../public/images/iconNol.svg';
 
 export default function NotFound() {
   return (
@@ -11,14 +13,17 @@ export default function NotFound() {
       <Header/>
         <div className="">
           <div className="bg-slate-950 flex overflow-hidden -mb-32">
-            <div className="relative w-full">
-              <Image src={RGradient} className="float-right"/>
-              <Image src={LGradient} className=""/>
+            <div className="relative w-full mb-12">
+              <Image src={RGradient} className="float-right z-[0]"/>
+              <Image src={LGradient} className=" mt-24 z-[1]"/>
             </div>
-            <div className="absolute w-full py-48 mt-24">
-              <div className="flex flex-col gap-16 items-center relative z-[2]">
-                <h1 className={`${font.Clash_display_d6bold} text-red-500 font-l text-center`}> 4 4</h1>
-                <p className="text-center text-white">Sersow is a project showcase app for uploading and showcasing their project work, interact with others, seek ideas and find collaborators or teams.</p>
+            <div className="absolute w-full py-24 mt-2">
+              <div className="flex flex-col gap-12  items-center relative z-[2]">
+                <h1 className={`${font.Clash_display_d1bold} text-white flex items-center text-center`}> 4 <Image src={IconNol}/> 4</h1>
+                <div className="flex flex-col gap-4 item-center relative z-[3]">
+                <h2 className={`${font.Clash_display_h1medium} text-center text-white`}>Congrats! Your just find an easter egg</h2>
+                <p className={`${font.Clash_display_h5medium} text-center text-gray-500`}>Nah,just kidding. You're lost within the mist.</p>
+                </div>
               </div>
             </div>  
           </div>
