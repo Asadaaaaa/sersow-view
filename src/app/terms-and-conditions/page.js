8fd02ffd5e32e74de77e7fa120710cb33217b4a3
Rafi <rafifajrul77@gmail.com>
2023-05-31 09:25:41 +0700

    feat: terms and condition page

--- src/app/terms-and-conditions/page.js
+++ src/app/terms-and-conditions/page.js
@@ -1,7 +1,16 @@
-import ComingSoon from "../coming-soon/Page";
-
+import font from "../font.module.css";
+import TermCondition from "./TermCondition";
+import CookiesPolicy from "./CookiesPolicy";
+import Header from "../coming-soon/Header";
+import Footer from "../Footer";
 export default function TaC() {
   return (
-    <ComingSoon />
+   <main  className="bg-slate-900">
+   <Header/>
+    <p className={`${font.Clash_display_h1medium} text-white text-center mt-12`}>Terms and Conditions</p>
+    <TermCondition/>
+    <CookiesPolicy/>
+    <Footer/>
+   </main>
   );
 }
