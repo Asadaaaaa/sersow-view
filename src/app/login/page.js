aba2634554954e061888633ecbfd15eb23379060
zkazharan <azka.azharan@gmail.com>
2023-05-31 10:28:07 +0700

    Feat: Login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,7 +5,7 @@ export default function Login() {
   return (
 		<main>
 			<Header />
-      <div className="bg-slate-950 relative overflow-hidden py-28">
+      <div className="bg-slate-950 relative overflow-hidden py-28 min-h-[calc(100vh-145.39px)]">
 				<div className="bg-gradient"></div>
         <div className="flex flex-col gap-16 items-center relative z-[2]">
 					<Form />
