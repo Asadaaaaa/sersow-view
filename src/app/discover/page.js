1241119657462c4b81de3ccf8d5d6bcc15acbc7f
zkazharan <azka.azharan@gmail.com>
2023-06-02 02:08:07 +0700

    Initiate discover page

--- src/app/discover/page.js
+++ src/app/discover/page.js
@@ -1,7 +1,20 @@
-import ComingSoon from "../coming-soon/Page";
+"use client";
+
+import { useState } from "react";
+
+import Main from "./Main";
+import Layout from "@/components/main/Layout";
+import { ContentFilter } from "@/components/main/discover/Context";
 
 export default function Discover() {
+
+  const [contentFilter, setContentFilter] = useState(0);
+
   return (
-    <ComingSoon />
+    <ContentFilter.Provider value={{ contentFilter, setContentFilter }} >
+      <Layout isLogin={true} page={"Discover"}>
+        <Main />
+      </Layout>
+    </ContentFilter.Provider>
   );
 }
