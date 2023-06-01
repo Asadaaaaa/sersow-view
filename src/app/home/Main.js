b2c7ec5f4b4d71034d9d796c10983f5cb9485a3e
zkazharan <azka.azharan@gmail.com>
2023-06-02 02:07:42 +0700

    Initiate Home Page

---
+++ src/app/home/Main.js
@@ -0,0 +1,9 @@
+import BgGradient from "@/components/main/BgGradient";
+
+export default function Main() {
+  return (
+    <div className="w-[1016px] relative h-[5000px]">
+      <BgGradient />
+    </div>
+  );
+}
