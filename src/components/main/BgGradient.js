363ff382c7b8eee6fe5666f24d280b5b58d4acb4
zkazharan <azka.azharan@gmail.com>
2023-06-02 02:10:51 +0700

    Initiate main layout

---
+++ src/components/main/BgGradient.js
@@ -0,0 +1,5 @@
+export default function BgGradient() {
+  return (
+    <div className="w-full h-full fixed bgGradient -z-10" style={{ background: "linear-gradient(179.45deg, #0F172A 12.54%, #020617 99.52%)" }}></div>
+  );
+}
