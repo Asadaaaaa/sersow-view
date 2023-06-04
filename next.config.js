963a5c6a16f169a0e322ed37c0ccdf7bca2aee9a
zkazharan <azka.azharan@gmail.com>
2023-06-05 01:14:23 +0700

    allow image host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'stg.api.sersow.otech.id',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
