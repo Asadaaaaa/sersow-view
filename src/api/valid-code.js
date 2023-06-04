e9f329ac51e682f719abc0030c2eb196df3db824
zkazharan <azka.azharan@gmail.com>
2023-06-05 01:14:02 +0700

    add res

--- src/api/valid-code.js
+++ src/api/valid-code.js
@@ -1,6 +1,6 @@
 export default async function validCode(regAuth, code) {
   try {
-    await fetch(process.env.NEXT_PUBLIC_HOST + "/" + process.env.NEXT_PUBLIC_VERSION + "/auth/valid-code", {
+    const res = await fetch(process.env.NEXT_PUBLIC_HOST + "/" + process.env.NEXT_PUBLIC_VERSION + "/auth/valid-code", {
       method: 'POST',
       headers: {
         'Accept': '*/*',
@@ -22,7 +22,7 @@ export default async function validCode(regAuth, code) {
           if (res.err.data.code === -1) {
             return {status: "-1"};
           } else if (res.err.data.code === -2) {
-            return {status: "-2"}
+            return {status: "-2"};
           }
         } else if (res.err.type === "validator") {
           return {status: "validator"};
