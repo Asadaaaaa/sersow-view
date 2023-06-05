317a0144524993c6ffef88745464ab9a224cde92
zkazharan <azka.azharan@gmail.com>
2023-06-05 21:12:32 +0700

    add settings header content

--- src/components/main/header/HeaderContent.js
+++ src/components/main/header/HeaderContent.js
@@ -1,11 +1,11 @@
 "use client";
 
-import { FaSearch } from 'react-icons/fa';
 import { usePathname } from 'next/navigation';
 
-import font from '@/app/font.module.css';
+import Searchbar from '@/components/main/header/Searchbar';
 import BackButton from '@/components/main/profile/BackButton';
 import HomeToggleFilter from '@/components/main/home/ToggleFilter';
+import SettingsToggleFilter from '@/components/main/settings/ToggleFilter';
 import DiscoverToggleFilter from '@/components/main/discover/ToggleFilter';
 
 export default function HeaderContent({ isLogin }) {
@@ -25,16 +25,16 @@ export default function HeaderContent({ isLogin }) {
           <BackButton />
         )
       }
-      <div className="relative">
-        <span className="absolute flex justify-end items-center w-[54px] h-full pr-3">
-          <FaSearch className="w-[18px] h-[18px] text-white" />
-        </span>
-        <input 
-          type="text" 
-          placeholder="Search..."
-          className={`${font.Satoshi_c1regular} w-56 bg-slate-800 text-white pl-[54px] pr-[13px] py-[13px] border-solid border-slate-700 border-[1px] rounded-xl transition-all ease-linear hover:border-slate-500 focus:outline-none focus:border-white focus:w-[312px]`} 
-        />
-      </div>
+      {
+        (page === "Settings") && (
+          <SettingsToggleFilter />
+        )
+      }
+      {
+        (page !== "Settings") && (
+          <Searchbar />
+        )
+      }
       {
         page === "Discover" && (
           <DiscoverToggleFilter />
