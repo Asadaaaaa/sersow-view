38e42efee26894f77a47850ff3f5d135f19fa53c
zkazharan <azka.azharan@gmail.com>
2023-06-05 18:19:21 +0700

    init follow

--- src/app/(main)/profile/Main.js
+++ src/app/(main)/profile/Main.js
@@ -1,22 +1,26 @@
 "use client";
 
+import Error from 'next/error';
 import Image from 'next/image';
 import { Loading } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 import { getCookie, deleteCookie } from 'cookies-next';
-import { useState, useEffect, useContext } from 'react';
-import { FaLink, FaEnvelope, FaRegEnvelope, FaRegCalendarAlt } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { FaUserEdit, FaLink, FaEnvelope, FaRegEnvelope, FaRegCalendarAlt } from 'react-icons/fa';
 
 import font from '@/app/font.module.css';
 import BgGradient from '@/components/main/BgGradient';
-import { Username } from '@/components/main/LoginContext';
 import styles from '@/components/main/profile/profile.module.css';
 
 import Profile from '@/api/profile/profile';
+import Follow from '@/api/activity/user/follow';
+import Unfollow from '@/api/activity/user/unfollow';
 
-export default function Main() {
+export default function Main({ username }) {
 
-  const { username } = useContext(Username);
+  const router = useRouter();
 
+  const [isFollowed, setIsFollowed] = useState(false);
   const [dataProfile, setDataProfile] = useState(null);
 
   function formatTimestamp(timestamp) {
@@ -27,9 +31,31 @@ export default function Main() {
     return formattedDate;
   }
 
+  async function follow() {
+    const res = await Follow(dataProfile.id, getCookie("auth"));
+
+    if (res.status === "200") {
+      setIsFollowed(true);
+    } else if (res.status === "unauth") {
+      router.push("login");
+    }
+  }
+
+  async function unfollow() {
+    const res = await Unfollow(dataProfile.id, getCookie("auth"));
+
+    if (res.status === "200") {
+      setIsFollowed(false);
+    } else if (res.status === "unauth") {
+      router.push("login");
+    }
+  } 
+
   useEffect(() => {
     async function getProfile() {
-      const res = await Profile(getCookie("auth"), username);
+      let res;
+
+      res = await Profile(username, getCookie("auth"));
 
       if (res.status === "200") {
         return res;
@@ -38,16 +64,19 @@ export default function Main() {
         deleteCookie("refreshAuth");
 
         location.reload();
+      } else if (res.status === "notfound") {
+        router.push("user-not-found")
+      } else {
+        throw new Error({statusCode: 500});
       }
     }
 
     async function fetchData() {
       const res = await getProfile();
 
-      console.log(res.data);
-
       if (res.status === "200") {
         setDataProfile(res.data);
+        setIsFollowed(res.data.isFollowed);
       }
     }
     fetchData();
@@ -72,7 +101,30 @@ export default function Main() {
                   </div>
                   <div className="flex justify-center">
                     <div className="py-1 px-2">
-                      <button className={`${font.Satoshi_c2bold} w-[136px] py-2 text-slate-700 bg-slate-200 rounded-3xl`}>Follow</button>
+                      {
+                        dataProfile.isMyProfile ? (
+                          <button className={`${font.Satoshi_c2bold} flex justify-center gap-2 w-[136px] py-2 text-white bg-gradient-to-b from-purple-500 to to-violet-600 rounded-3xl`}>
+                            <FaUserEdit className="w-4 h-4" />
+                            <h3>Edit</h3>
+                          </button>
+                        ) : (
+                          isFollowed ? (
+                            <button 
+                              className={`${font.Satoshi_c2bold} w-[136px] py-2 text-slate-200 border-solid border-slate-700 border-[1px] rounded-3xl`}
+                              onClick={unfollow}
+                            >
+                              Following
+                            </button>
+                          ) : (
+                            <button 
+                              className={`${font.Satoshi_c2bold} w-[136px] py-2 text-slate-700 bg-slate-200 border-solid border-white border-[1px] rounded-3xl`}
+                              onClick={follow}
+                            >
+                              Follow
+                            </button>
+                          )
+                        )
+                      }
                     </div>
                     <div className="p-[6px]">
                       <button className="border-solid border-slate-300 border-[1px] rounded-full p-2">
@@ -111,12 +163,16 @@ export default function Main() {
                   <div className="flex flex-col gap-1">
                     <div className="flex gap-2 items-center">
                       <div><FaEnvelope className="w-3 h-3 text-slate-600" /></div>
-                      <h4 className={`${font.Satoshi_c2regular} text-slate-400`}>dummyemmail@upi.edu</h4>
+                      <h4 className={`${font.Satoshi_c2regular} text-slate-400`}>{dataProfile.email_upi}</h4>
                     </div>
-                    <div className="flex gap-2 items-center">
-                      <div><FaRegEnvelope className="w-3 h-3 text-slate-600" /></div>
-                      <h4 className={`${font.Satoshi_c2regular} text-slate-400`}>dummyemail@gmail.com</h4>
-                    </div>
+                    {
+                      dataProfile.email_gmail && (
+                        <div className="flex gap-2 items-center">
+                          <div><FaRegEnvelope className="w-3 h-3 text-slate-600" /></div>
+                          <h4 className={`${font.Satoshi_c2regular} text-slate-400`}>{dataProfile.email_gmail}</h4>
+                        </div>
+                      )
+                    }
                     {
                       dataProfile.website && (
                         <div className="flex gap-2 items-center">
@@ -134,13 +190,13 @@ export default function Main() {
                       </div>
                       <hr className="w-2 border-slate-700" />
                       <div className={`${font.Satoshi_c2regular} text-slate-400`}>
-                        <span className={`${font.Satoshi_c2medium} text-white`}>4{dataProfile.total_following}</span>
+                        <span className={`${font.Satoshi_c2medium} text-white`}>{dataProfile.total_following}</span>
                         {" "}following
                       </div>
                     </div>
                     <div className="flex items-center gap-1">
                       <div className={`${font.Satoshi_c2regular} text-slate-400`}>
-                        <span className={`${font.Satoshi_c2medium} text-white`}>1 (dummy)</span>
+                        <span className={`${font.Satoshi_c2medium} text-white`}>{dataProfile.total_project}</span>
                         {" "}projects
                       </div>
                     </div>
@@ -151,7 +207,7 @@ export default function Main() {
                   </div>
                 </>
               ) : (
-                <div className="pt-12">
+                <div className="pt-12 flex justify-center">
                   <Loading>
                     Loading
                   </Loading>
