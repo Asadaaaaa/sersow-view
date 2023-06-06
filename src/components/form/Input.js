22b8e308be91c898e095157460379462b6e52626
zkazharan <azka.azharan@gmail.com>
2023-06-06 21:36:05 +0700

    add spellcheck attributes

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -2,7 +2,8 @@ import font from '@/app/font.module.css';
 
 export default function Input({ placeholder, type, value, onChange, maxLength, error, onFocus }) {
   return (
-    <input 
+    <input
+      spellCheck={"false"} 
       className={`${font.Satoshi_b2regular} text-white w-[350px] ${placeholder === "Email" ? "pl-6 pr-28" : "px-6"} py-3 bg-transparent outline-none border-solid border-2 focus:border-white rounded-xl ${error ? "border-rose-400 hover:border-rose-400": "border-slate-700 hover:border-slate-500"}`} 
       type={type} 
       placeholder={placeholder}
--- src/components/main/settings/Input.js
+++ src/components/main/settings/Input.js
@@ -3,6 +3,7 @@ import font from '@/app/font.module.css';
 export default function Input({ placeholder, type, value, onChange, maxLength, error, onFocus }) {
   return (
     <input 
+      spellCheck={"false"}
       className={`${font.Satoshi_c1regular} py-3 px-6 border-solid text-white border-[1px] bg-transparent outline-none focus:border-white rounded-lg ${error ? "border-rose-400 hover:border-rose-400": "border-slate-700 hover:border-slate-500"}`} 
       type={type} 
       placeholder={placeholder}
