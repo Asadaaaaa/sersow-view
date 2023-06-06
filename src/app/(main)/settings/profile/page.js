2701924d06fc0b7ec04b4217c29ba678d78dff74
zkazharan <azka.azharan@gmail.com>
2023-06-07 01:33:29 +0700

    add py

--- src/app/(main)/settings/profile/page.js
+++ src/app/(main)/settings/profile/page.js
@@ -156,7 +156,7 @@ export default function Profile() {
   return (
     <div className="flex justify-center h-full">
       <div className="w-[456px] h-full pt-24">
-        <div className="flex flex-col gap-6 pt-12">
+        <div className="flex flex-col gap-6 py-12">
           <CardContainer>
             <CardTitle title={"Avatar"} subtitle={"Unleash your good looking avatar or just use our amazing default avatar."} />
             <CardSubtitle text={"Just drag and drop your avatar or manually choose it"} />
