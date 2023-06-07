cfd58374ad61f1d43dd9af6feb4d5d121a360855
zkazharan <azka.azharan@gmail.com>
2023-06-07 08:36:37 +0700

    update textarea

--- src/components/main/settings/Textarea.js
+++ src/components/main/settings/Textarea.js
@@ -2,13 +2,12 @@ import TextareaAutosize from 'react-textarea-autosize';
 
 import font from '@/app/font.module.css';
 
-export default function Textarea({ placeholder, type, value, onChange, maxLength, error, onFocus }) {
+export default function Textarea({ minRows, placeholder, value, onChange, maxLength, error, onFocus }) {
   return (
     <TextareaAutosize
-      minRows={1}
+      minRows={minRows}
       spellCheck={"false"}
       className={`${font.Satoshi_c1regular} !resize-none py-3 px-6 border-solid text-white border-[1px] bg-transparent outline-none focus:border-white rounded-lg ${error ? "border-rose-400 hover:border-rose-400": "border-slate-700 hover:border-slate-500"}`} 
-      type={type} 
       placeholder={placeholder}
       maxLength={maxLength}
       value={value}
