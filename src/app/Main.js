87b68edd55595a5d929cb9054c45f0b1f6661243
Innaka <innakadyleexd@gmail.com>
2023-06-08 14:51:24 +0700

    header and footer

--- src/app/Main.js
+++ src/app/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
     <div className="relative w-full" id="main">
       <Image src="/images/GalaxyBackground.png" width={1280} height={910} className="xl:w-screen xl:h-auto xl:object-contain lg:w-screen lg:h-auto lg:object-contain md:w-screen md:h-auto md:object-contain sm:w-auto sm:h-[1280px] sm:object-cover ss:w-auto ss:h-[700px] ss:object-cover" />
       <div className="w-full h-full bg-Gradient1 absolute top-0">
-        <div className="flex flex-col h-full w-full xl:items-start lg:items-start md:items-start xl:text-start lg:text-start md:text-start justify-end absolute xl:gap-24 xl:py-20 xl:-top-[36vh] xl:left-24 xl:translate-x-0 lg:gap-20 lg:py-16 lg:-top-[18vh] lg:left-20 lg:translate-x-0 md:gap-10 md:py-12 md:-top-[25vh] md:left-16 md:translate-x-0 sm:gap-12 sm:py-10 sm:-top-[45vh] sm:left-[50%] sm:translate-x-[-50%] sm:items-center sm:text-center ss:gap-10 ss:py-10 ss:-top-[25vh] ss:left-[50%] ss:translate-x-[-50%] ss:items-center ss:text-center">
+        <div className="flex flex-col h-full w-full xl:items-start lg:items-start md:items-start xl:text-start lg:text-start md:text-start justify-end absolute xl:gap-24 xl:py-20 xl:-top-[36vh] xl:left-24 xl:translate-x-0 lg:gap-20 lg:py-16 lg:-top-[18vh] lg:left-20 lg:translate-x-0 md:gap-10 md:py-12 md:-top-[25vh] md:left-16 md:translate-x-0 sm:gap-12 sm:py-10 sm:-top-[56vh] sm:left-[50%] sm:translate-x-[-50%] sm:items-center sm:text-center ss:gap-10 ss:py-10 ss:-top-[25vh] ss:left-[50%] ss:translate-x-[-50%] ss:items-center ss:text-center">
           <div>
             <h1 className={`${font.Clash_display_d4bold} text-white`}>Howdy Savvy!</h1>
             <p className={`${font.Clash_display_h5regular} text-white`}>Discover enormous projects at our showcase</p>
