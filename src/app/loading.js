009928e3508a7c7e57c2d5437f6deb2ff0bd141d
Rafi <rafifajrul77@gmail.com>
2023-06-16 00:21:55 +0700

    feat: loading screen

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import styles from "./root.module.css";
 
-import sersow from "../../public/images/Sersow.png";
+import sersow from "../../public/images/Loading/icon.svg";
 
 export default function Loading() {
   return (
     <main className="w-screen h-screen bg-slate-950 flex justify-center items-center">
-      <Image src={sersow} alt="sersow" className="w-32 h-auto" />
+      <div>
+        <Image
+          src={sersow}
+          alt="sersow"
+          className={`${styles.loading} w-[120px] h-auto`}
+        />
+      </div>
     </main>
   );
 }
---
+++ src/app/loading/page.js
@@ -0,0 +1,9 @@
+import Loading from "../loading";
+
+export default function page() {
+  return (
+    <main>
+      <Loading />
+    </main>
+  );
+}
