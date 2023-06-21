b5e9d5819ab63b956a8340618439d4c402abce0b
ta-lim <muhamadtalim@gmail.com>
2023-06-21 10:47:20 +0700

    delete: unuse component

--- src/components/main/discover/CardContainer.js
+++
@@ -1,7 +0,0 @@
-export default function CardContainer({ children }) {
-  return (
-    <div className="flex justify-between items-center w-[400px] h-24 p-6 gap-4 bg-slate-900 rounded-lg ">
-      {children}
-    </div>
-  );
-}
