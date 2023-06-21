04aa81d7f1818c16f29f23eae9470404cf4adb0c
zkazharan <azka.azharan@gmail.com>
2023-06-21 12:45:28 +0700

    Add Edit Title

--- src/components/main/project/ProjectTitle.js
+++ src/components/main/project/ProjectTitle.js
@@ -8,7 +8,17 @@ export default function ProjectTitle() {
 	const pathname = usePathname();
 	const path = pathname.split("/");
 
+	let title = "";
+
+	if (path.length === 2) {
+		title = "Untilted Project";
+	} else if (path.length === 3) {
+		title = "Details Project";
+	} else if (path.length === 4) {
+		title = "Edit Project";
+	}
+
 	return (
-		<h1 className={`${font.Satoshi_h5bold} text-white`}>{ path.length === 3 ? "Details Project" : "Untilted Project" }</h1>
+		<h1 className={`${font.Satoshi_h5bold} text-white`}>{ title }</h1>
 	);
 }
