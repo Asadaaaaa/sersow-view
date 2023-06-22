1b5ad2c5faf2ee966560a464bfeaec67f4cb9f5f
zkazharan <azka.azharan@gmail.com>
2023-06-23 01:39:11 +0700

    Add handling when content not found

--- src/app/(main)/discover/Project.js
+++ src/app/(main)/discover/Project.js
@@ -19,6 +19,7 @@ export default function Project() {
   const [dataProjectEven, setDataProjectEven] = useState([]);
   const [dataCategory, setDataCategory] = useState([{ name: "All" }]);
   const [filterCategory, setFilterCategory] = useState({ name: "All" });
+  const [loadingPage, setLoadingPage] = useState(true);
 
   async function fetchData() {
     const res = await ProjectTrends(getCookie("auth"));
@@ -26,6 +27,7 @@ export default function Project() {
     if (res) {
       if (res.status === "200") {
         setDataProject(res.data);
+        setLoadingPage(false);
       }
     }
   }
@@ -83,9 +85,6 @@ export default function Project() {
         if (res.status === "unauth") {
           location.reload();
         }
-        if (res.status === "notfound") {
-          router.push("not-found");
-        }
       }
     }
 
@@ -95,55 +94,61 @@ export default function Project() {
 
   return (
     <div className="flex flex-col justify-center px-24 mt-24 gap-12 text-white">
-      <div className={`${styles.discoverCategory} flex w-full min-w-full pb-3 pt-12 gap-4 bg-gradient-b  from-slate-900/60 to-slate-900/20 backdrop-blur-md overflow-x-auto `} >
-        {dataCategory.map((item) => (
-          <div
-            className={
-              "flex justify-center items-center h-8 border border-slate-500 rounded-full py-2 px-4 whitespace-nowrap cursor-pointer " +
-              (item.name === filterCategory.name
-                ? "bg-slate-200 text-slate-950"
-                : "bg-slate-900 text-slate-200")
-            }
-            onClick={() => setFilterCategory(item)}
-            key={item.id}
-          >
-            {item.name}
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-col w-full h-full gap-6 ">
-        {
-          !projectContent && (
-            <div className="flex items-center gap-2">
-              <FaChartLine fill="white" />
-              <p className={`${font.Satoshi_b2medium} text-white`}>TRENDS</p>
-            </div>
-          )
-        }
-        <div className="flex flex-wrap justify-center gap-10">
-        {dataProject.length !== 0 ? (
-          <>
-          <div className="flex flex-col gap-6 items-start">
-            {dataProjectEven.map((item, index) => (
-                  <ContainerProject index={index} data={item} key={item.id} />
-              ))
-            }
-          </div>
-
-          <div className="flex flex-col gap-6 items-start">
-            {dataProjectOdd.map((item, index) => (
-                  <ContainerProject index={index} data={item} key={item.id}/>
-              ))
-            }
-          </div>
-          </>
-        ) : (
+      {
+        loadingPage ? (
           <div className="pt-12 flex justify-center">
             <Loading />
           </div>
-        )}
-        </div>
-      </div>
+        ) : dataProject.length !== 0 ? (
+          <>
+            <div className={`${styles.discoverCategory} flex w-full min-w-full pb-3 pt-12 gap-4 bg-gradient-b from-slate-900/60 to-slate-900/20 backdrop-blur-md overflow-x-auto `} >
+              {dataCategory.map((item) => (
+                <div
+                  className={
+                    "flex justify-center items-center h-8 border border-slate-500 rounded-full py-2 px-4 whitespace-nowrap cursor-pointer " +
+                    (item.name === filterCategory.name
+                      ? "bg-slate-200 text-slate-950"
+                      : "bg-slate-900 text-slate-200")
+                  }
+                  onClick={() => setFilterCategory(item)}
+                  key={item.id}
+                >
+                  {item.name}
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-col w-full h-full gap-6 ">
+              {
+                !projectContent && (
+                  <div className="flex items-center gap-2">
+                    <FaChartLine fill="white" />
+                    <p className={`${font.Satoshi_b2medium} text-white`}>TRENDS</p>
+                  </div>
+                )
+              }
+              <div className="flex flex-wrap justify-center gap-10">
+                <div className="flex flex-col gap-6 items-start">
+                  {dataProjectEven.map((item, index) => (
+                      <ContainerProject index={index} data={item} key={item.id} />
+                    ))
+                  }
+                </div>
+
+                <div className="flex flex-col gap-6 items-start">
+                  {dataProjectOdd.map((item, index) => (
+                      <ContainerProject index={index} data={item} key={item.id}/>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center pt-12">
+            <h1 className={`${font.Satoshi_h2medium} text-white`}>Project Not Found!</h1>
+          </div>
+        )
+      }
     </div>
   );
 }
--- src/app/(main)/discover/Users.js
+++ src/app/(main)/discover/Users.js
@@ -15,16 +15,16 @@ export default function User() {
   const { userContent } = useContext(ContentFilter);
 
   const [dataProfile, setDataProfile] = useState([]);
+  const [loadingPage, setLoadingPage] = useState(true);
 
   async function getUserTrends() {
     const res = await UserTrends(getCookie("auth"));
 
     if (res.status === "200") {
       setDataProfile(res.data);
+      setLoadingPage(false);
     } else if (res.status === "unauth") {
       location.reload();
-    } else if (res.status === "notfound") {
-      router.push("user-not-found");
     }
   }
 
@@ -43,33 +43,39 @@ export default function User() {
   return (
     <div className="flex justify-center h-full">
       <div className="flex w-full h-full pt-24">
-        <div className="flex flex-col w-full h-full px-20 py-8">
-          {
-            !userContent && (
-              <div className="flex items-center gap-2 p-4">
-                <FaChartLine fill="white" />
-                <p className={`${font.Satoshi_b2medium} text-white`}>TRENDS</p>
-              </div>
-            )
-          }
-          {dataProfile.length !== 0? (
-            <div className="flex flex-wrap justify-center gap-6 p-0">
-              {dataProfile.map((item, index) => {
-                return(
-                <UserContainer index={item.id} style={"h-24 p-6"}>
-                  <Avatar username={item.username} name={item.nameSubstr} image={item.image} />
-                  <ButtonFollow id={item.id} isMyProfile={item.isMyProfile} isFollowed={item.isFollowed} key={item.id} />
-                </UserContainer>
-                )
-              }
-              )}
-            </div>
-          ) : (
-            <div className="pt-12 flex justify-center">
+        {
+          loadingPage ? (
+            <div className="w-full pt-12 flex justify-center">
               <Loading />
             </div>
-          )}
-        </div>
+          ) : dataProfile.length !== 0 ? (
+            <div className="flex flex-col w-full h-full px-20 py-8">
+              {
+                !userContent && (
+                  <div className="flex items-center gap-2 p-4">
+                    <FaChartLine fill="white" />
+                    <p className={`${font.Satoshi_b2medium} text-white`}>TRENDS</p>
+                  </div>
+                )
+              }
+              <div className="flex flex-wrap justify-center gap-6 p-0">
+                {dataProfile.map((item, index) => {
+                  return(
+                  <UserContainer index={item.id} style={"h-24 p-6"}>
+                    <Avatar username={item.username} name={item.nameSubstr} image={item.image} />
+                    <ButtonFollow id={item.id} isMyProfile={item.isMyProfile} isFollowed={item.isFollowed} key={item.id} />
+                  </UserContainer>
+                  )
+                }
+                )}
+              </div>
+            </div>
+          ) : (
+            <div className="w-full pt-12 flex justify-center">
+              <h1 className={`${font.Satoshi_h2medium} text-white`}>User Not Found!</h1>
+            </div>
+          )
+        }
       </div>
     </div>
   );
